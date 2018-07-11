1bc14acbf3a9639d544a72f3c9008db44fa3d9d9
Ronaldo Umana <ronaldo.ur@gmail.com>
2018-07-11 10:58:40 -0600

    removed sudo from script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 eval $(aws ecr get-login --no-include-email --region us-east-1 | sed 's|https://||')
-sudo docker build -t docker-app .
-sudo docker tag docker-app 492864460344.dkr.ecr.us-east-1.amazonaws.com/docker-app:latest
-sudo docker push 492864460344.dkr.ecr.us-east-1.amazonaws.com/docker-app:latest
+docker build -t docker-app .
+docker tag docker-app 492864460344.dkr.ecr.us-east-1.amazonaws.com/docker-app:latest
+docker push 492864460344.dkr.ecr.us-east-1.amazonaws.com/docker-app:latest
