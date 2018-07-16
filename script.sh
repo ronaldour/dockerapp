02db16eda1b851b3d2ab5adf1427fca77aa01264
Mario Ronaldo Umana <ronaldo.ur@gmail.com>
2018-07-16 08:23:00 -0600

    changed something again

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#script for building and pushing docker image ADS
+#script for building and pushing docker image ADSasdf
 eval $(aws ecr get-login --no-include-email --region us-east-1 | sed 's|https://||')
 docker build -t docker-app .
 docker tag docker-app 492864460344.dkr.ecr.us-east-1.amazonaws.com/docker-app:latest
